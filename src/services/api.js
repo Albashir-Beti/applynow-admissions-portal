2b0f69378313eb1a82f605e51671376a4a63e9c9
albashir-beti <albashirmaigaribeti7@gmail.com>
2026-09-19 18:07:45 +0100

    Prepare ApplyNow for deployment

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-// Create one reusable Axios instance for our mock API
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:3001",
 });
 
 export default api;
